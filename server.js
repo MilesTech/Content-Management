05d661cdfbddfa7aa8526000dc759ac7223545f3
Joe Zeoli <jzeoli@mam-jzeoli.miles.office>
2015-02-03 16:47:12 -0500

    Updated server and dependencies

--- server.js
+++ server.js
@@ -1,17 +1,109 @@
 /*Define dependencies.*/
-
-var express=require("express");
+var express = require("express");
+var app = express();
 var multer  = require('multer');
-
-var app=express();
+var passport = require('passport')
+var GoogleStrategy = require('passport-google').Strategy;
+var mongoose = require('mongoose');                  
+var morgan = require('morgan');    
+var bodyParser = require('body-parser'); 
+var methodOverride = require('method-override'); 
+  
 var done=false;
 
 
+/*=================Config=============*/
+ mongoose.connect('mongodb://root:Raqob4anoj@proximus.modulusmongo.net:27017/bovutY7x'); 
+app.use(express.static(__dirname + '/public'));  
+app.use(morgan('dev')); 
+app.use(bodyParser.urlencoded({'extended':'true'})); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(methodOverride());
 
-app.use(express.static('public'));
+
+
+  var Todo = mongoose.model('Todo', {
+        text : String
+    });
+
+
+
+//Routes
+
+ app.get('/api/todos', function(req, res) {
+
+        // use mongoose to get all todos in the database
+        Todo.find(function(err, todos) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(todos); // return all todos in JSON format
+        });
+    });
+
+    // create todo and send back all todos after creation
+    app.post('/api/todos', function(req, res) {
+
+        // create a todo, information comes from AJAX request from Angular
+        Todo.create({
+            text : req.body.text,
+            done : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+
+    });
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*Configure the multer.*/
-app.use(multer({ dest: './uploads/',
+
+/*app.use(multer({ dest: './uploads/',
  rename: function (fieldname, filename) {
     return filename+Date.now();
   },
@@ -24,7 +116,26 @@ onFileUploadComplete: function (file) {
 }
 }));
 
-/*Handling routes.*/
+
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost/auth/google/return',
+    realm: 'http://localhost/'
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+    });
+  }
+));
+
+
+app.get('/auth/google', passport.authenticate('google'));
+
+
+app.get('/auth/google/return', 
+  passport.authenticate('google', { successRedirect: '/',
+                                    failureRedirect: '/login' }));
 
 
 
@@ -32,6 +143,9 @@ app.get('/',function(req,res){
       res.sendfile("index.html");
 });
 
+app.get('/login',function(req,res){
+      res.sendfile("index.html");
+});
 
 
 app.post('/api/photo',function(req,res){
@@ -39,7 +153,7 @@ app.post('/api/photo',function(req,res){
     console.log(req.files);
     res.end("File uploaded.");
   }
-});
+});*/
 
 
 
